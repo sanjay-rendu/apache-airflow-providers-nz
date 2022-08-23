026b39de0cfdd146a3ead12f8ff1ea6b4537c9a4
Sanjay Renduchintala <san8055@gmail.com>
2022-08-22 19:45:57 -0700

    Delete apache-airflow-providers-nz directory

--- apache-airflow-providers-nz/provider_info.py
+++
@@ -1,9 +0,0 @@
-def get_provider_info():
-    return {
-        "package-name": "apache-airflow-providers-nz",
-        "name": "netezza",
-        "description": "Airflow Hook for Netezza",
-        "hook-class-names": [
-            "apache-airflow-providers-nz.hooks.netezza.NetezzaHook",
-        ],
-    }
